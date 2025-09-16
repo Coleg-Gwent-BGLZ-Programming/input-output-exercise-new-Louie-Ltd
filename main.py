e274b9744d1779ffc1d662d60c8c47cef9268ca5
oscar <10320235@coleggwent.ac.uk>
2025-09-16 12:00:50 +0100

    Update main.py
    this is the thing

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# Starter file for the Smoothie Shop Calculator exercise
 
-print("Welcome to the Smoothie Shop!")
+print("Welcome to the smoothie shop")
+price = 3.50
+reusable_cup = None
+options =("yes", "no")
+user_name = input(" please input your name ")
+smoothie_amount = int(input("please input the amount of smoothies you wish to buy"))
+while reusable_cup not in options:
+  reusable_cup = input("would you like a reusable cup yes or no :")
+if reusable_cup == "yes":
+    total = smoothie_amount * price + 1
+    print(f"thank you for your purchase {user_name} you were charged £{total}0 ")
 
-# TODO: Ask the user for their name using input()
+else:
+    total = smoothie_amount * price
+    print(f"thank you for your purchase {user_name} you were charged £{total}0 ")
 
-
-# TODO: Ask how many smoothies they want to buy
-
-
-# TODO: Convert the number of smoothies to an integer
-
-
-# TODO: Calculate the total cost (each smoothie costs £3.50)
-
-
-# TODO: Display a message using a formatted string
-
-
-# OPTIONAL CHALLENGE:
-# Ask if the customer wants a reusable cup for £1.00 extra
-# Add the cost if they say yes
